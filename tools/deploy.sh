429c9a384b9e506a4e2035a795e1779a79baed84
ef4tless <963697159@qq.com>
2022-05-12 09:27:23 +0800

    测试6

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -104,8 +104,6 @@ flush() {
 deploy() {
   git config --global user.name "GitHub Actions"
   git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"
-  git remote rm origin
-  git remote add origin git@github.com:ef4tless/ef4tless.github.io.git
 
 
   git update-ref -d HEAD
