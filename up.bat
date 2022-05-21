e5d1ecbced9347fe4a74983e2e871a3dfceb7249
ef4tless <963697159@qq.com>
2022-05-21 13:48:01 +0800

    e4l4

--- up.bat
+++ up.bat
@@ -1,14 +1,15 @@
 @echo off
  
 title GIT一键提交
-color 3
 echo 当前目录是：%cd%
 echo;
- 
+gulp
+echo;
+
 echo 开始添加变更：git add .
 git add .
 echo;
- 
+
 set /p declation=输入提交的commit信息:
 git commit -m "%declation%"
 echo;
